742f917317ccd71c9eeea2928612feefd5e9234f
Ivan Derbenev <Ivan.Derbenev@nottingham.ac.uk>
2022-04-24 23:33:26 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from webbot import Browser
+web = Browser()
+web.go_to('https://ncc.leisurecloud.net/Connect/MRMLogin.aspx')
+web.type('Ivan.Derbenev@nottingham.ac.uk', into='Email Address', id='ctl00_MainContent_InputLogin')
+web.type('', into='Password', id='ctl00_MainContent_InputPassword') # specific selection
+web.click('Login', tag='span')  # you are logged in ^_^
