78f466541e3b63204459c17edff709d4351343ee
Ivan Derbenev <Ivan.Derbenev@nottingham.ac.uk>
2022-04-30 23:17:14 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-from selenium.webdriver.support.select import Select
-from selenium import webdriver
-import os
+from webbot import Browser
 import time
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-chrome_dir = os.path.join(basedir, "chromedriver")
-driver = webdriver.Chrome(chrome_dir)
-driver.get('https://ncc.leisurecloud.net/Connect/MRMLogin.aspx')
+web = Browser()
+web.go_to('https://ncc.leisurecloud.net/Connect/MRMLogin.aspx')
 """Login"""
-email_field = driver.find_element_by_id('ctl00_MainContent_InputLogin')
-email_field.clear()
-email_field.send_keys("Ivan.Derbenev@nottingham.ac.uk")
-password_field = self.driver.find_element_by_id('ctl00_MainContent_InputPassword')
-password_field.clear()
-password_field.send_keys("Param0n0v")
-driver.find_element_by_id('ctl00_MainContent_btnLogin').click()
-# web.type('Ivan.Derbenev@nottingham.ac.uk', into='Email Address', id='ctl00_MainContent_InputLogin')
-# web.type('Param0n0v', into='Password', id='ctl00_MainContent_InputPassword')
-# web.click('Login', tag='span')
+web.type('Ivan.Derbenev@nottingham.ac.uk', into='Email Address', id='ctl00_MainContent_InputLogin')
+web.type('***', into='Password', id='ctl00_MainContent_InputPassword')
+web.click('Login', tag='span')
 time.sleep(1)
 """Choose leisure centre"""
 # web.click(id='ctl00_MainContent__advanceSearchUserControl_SitesSimple')
-
+web.click(tag='option', text='Djanogly Leisure Centre')
+"""Click the button '7 days'"""
+web.click(id='ctl00_MainContent__advanceSearchUserControl__lnkBtnSevenDaysTime')
+time.sleep(3)
+"""Choose a class"""
+web.click(id='ctl00_MainContent__advanceSearchResultsUserControl_Classes_ctrl7_lnkActivitySelect_lg')
+time.sleep(1)
+"""Click 'Book'"""
+web.click(id='ctl00_MainContent_ClassStatus_ctrl0_btnBook')
+time.sleep(1)
+"""Click 'Book & Checkout'"""
+web.click(id='ctl00_MainContent_btnBasket')
+time.sleep(1)
+"""Click 'Pay with Card'"""
+web.click(id='ctl00_MainContent_btnPay2')
+time.sleep(2)
+"""Click 'pay now'"""
+web.click(text='Pay Now', tag='a')
